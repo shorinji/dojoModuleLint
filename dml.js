8677979788227748cb4bdca03cd3886bb740755f
Fredrik Fall <fredrik.fall@fronter.com>
2012-10-16 14:40:47 +0200

    Replaced uglify with acorn
    Switched to acorn since it provides a more intuitive AST to work with.
    Also made check for number of dependencies not count domReady and any
    css files at the end of the dependency list. This since the norm is not
    to put variables for these

--- dml.js
+++ dml.js
@@ -1,106 +1,124 @@
+#!/usr/bin/env node
 // Run me in node
 
-"use strict";
+(function() {
+    "use strict";
 
-var uglifyPath = "../UglifyJS/";
-var assert = require('assert'),
-	fs = require('fs'),
-	util = require('util'),
-	parser = require(uglifyPath + 'uglify-js').parser;
+    var pathToAcorn = '..';
 
-if(process.argv.length < 3) {
-	console.error("Usage: %s filename.js", "dml");
-	process.exit();
-}
-
-var filename = process.argv[2],
-	fileContents;
-
-try {
-	fileContents = fs.readFileSync(filename, "utf8");
-} catch(e) {
-	console.error("ERROR: %s", e);
-	process.exit();
-}
-
-//console.log( fileContents );
-/*
-var nextToken = parser.tokenizer(fileContents),
-	t = nextToken(),
-	start = t.pos,
-	end = t.endpos;
-
-while(start < end) {
-
-	console.log( util.inspect( t , true, null, true));
-
-	t = nextToken();
-	start = t.pos;
-	end = t.endpos;
-}
-process.exit();
-*/
+    var assert = require('assert'),
+        fs = require('fs'),
+        util = require('util'),
+        parser = require(pathToAcorn + '/acorn');
 
 
+    // start of global function definitions
+    function log(key, val, depth) {
+        if(!depth) {
+            depth = null;
+        }
+        console.log("\n%s%s",  (key ? (key + ': ') : '') , util.inspect(val, true, depth, true) );
+    }
 
-try {
-	var tree = parser.parse( fileContents );
-} catch(e) {
-	console.error("PARSE ERROR: '%s' [line:%s pos:%s col:%s]\n\nTry running jslint/jshint on the file!", e.message, e.line, e.pos, e.col);
-	process.exit();
-}
+    function assertDefineCall(stmt, str) {
 
-// display full abstract syntax tree
-//console.log( util.inspect(tree, true, null, true) );
+        assert(stmt.type == 'ExpressionStatement', str);
+        assert(stmt.expression.type == 'CallExpression', str);
+        assert(stmt.expression.callee.name == 'define', str);
+    }
+
+    function isStyleExpression(node) {
+        return (
+            node.type == 'BinaryExpression' &&
+            node.operator == '+' &&
+            node.left.left.value.indexOf('css') >= 0);
+    }
+
+    function die(x) {
+        if(x) {
+            log("", x, 1);
+        }
+        process.exit();
+    }
+    // end of global functions
 
 
-function isCallWithName(subtree, name) {
+    if(process.argv.length < 3) {
+        console.error("Usage: %s filename.js", "dml");
+        process.exit();
+    }
+    var filename = process.argv[2];
 
-	return (subtree[0] == 'stat' &&
-		subtree[1][0] == 'call' &&
-		subtree[1][1][1] == name);
-}
+    var fileContents;
+    try {
+        fileContents = fs.readFileSync(filename, "utf8");
+    } catch(e) {
+        console.error("ERROR: %s", e);
+        process.exit();
+    }
 
-function hasDefine(tree) {
-	var firstStatement = tree[1][0];
-	return isCallWithName(firstStatement, 'define');
-}
+    var tree;
+    try {
+        tree = parser.parse( fileContents );
+    } catch(e) {
+        console.error("PARSE ERROR: '%s' [line:%s pos:%s col:%s]\n\nTry running jslint/jshint on the file!", e.message, e.line, e.pos, e.col);
+        process.exit();
+    }
 
-var toplevel = tree[1];
-try{
+    // display full abstract syntax tree
+    //console.log( util.inspect(tree, true, null, true) );
 
-assert(tree[1].length > 0, "Empty file");
-assert( hasDefine(tree), "First statement must be define");
-assert(tree[1].length < 2, "Only one top-level statement (define)"); // code-convention
-var defineCall = tree[1][0][1][2];
 
-assert.equal(defineCall.length, 2, "define requires 2 arguments"); // code-convention
+    try {
 
-var defineDependencies = defineCall[0],
-	defineCallback = defineCall[1],
-	defineCallbackArgs = defineCallback[2];
+        assert(tree.end > 0, "Empty file");
+        
+        var firstStatement = tree.body[0];
 
-assert.equal(defineDependencies[0], 'array', 'First argument to define() must be array'); // code-convention
-assert.equal(defineCallback[0], 'function', 'Second argument to define must be function definition'); // code-convention
+        assertDefineCall(firstStatement, "First statement must be define");
+        assert(tree.body.length < 2, "Only one top-level statement (define)"); // code-convention
 
-var numDependencies = defineDependencies[1].length;
-var numCallbackArgs = defineCallbackArgs.length;
 
-assert.equal(numDependencies, numCallbackArgs, "Number of module dependencies does not match number of callback arguments! [" + numDependencies + " != " + numCallbackArgs + "]");
+        var defineCall = firstStatement.expression,
+            defineArgs = defineCall['arguments'];
 
-//console.log("\nXXX: %s ",  util.inspect(defineDependencies, true, null, true) );
+        assert.equal(defineArgs.length, 2, "define requires 2 arguments"); // code-convention
 
-defineDependencies[1].forEach(function(dep) {
-	// do some testing per dependency module or add the for later use
-});
+        var defineDependencies = defineArgs[0],
+            defineCallback = defineArgs[1],
+            defineCallbackArgs = defineArgs[1].params;
 
-//assert.equal(defineDependencies[1][0][0], 'string', 'Dependencies must be string'); // code-convention
+        assert.equal(defineDependencies.type, 'ArrayExpression', 'First argument to define() must be array'); // code-convention
+        assert.equal(defineCallback.type, 'FunctionExpression', 'Second argument to define must be function definition'); // code-convention
 
-//console.log("\nXXX2: %s ",  util.inspect(define[0], true, null, true) );
+        var numDependencies = defineDependencies.elements.length;
+        var numCallbackArgs = defineCallbackArgs.length;
 
-} catch(e) {
-	console.warn("Failure: %s", e);
-	process.exit();
-}
+        var needToCheckLast = true;
 
-console.log("Found nothing obvious wrong with your file");
+        // check for and remove "dom/ready!" from count and same for css
+        while( needToCheckLast  ) {
+            var dep = defineDependencies.elements[ numDependencies - 1];
+            if( dep.type == 'Literal' && dep.value == 'dojo/domReady!') {
+                numDependencies--;
+                continue;
+            }
+
+            // check for our concatenated (skinned) css files
+            if( isStyleExpression(dep) ) {
+                numDependencies--;
+                continue;
+            }
+            needToCheckLast = false;
+        }
+
+        assert.equal(numDependencies, numCallbackArgs, "Number of module dependencies does not match number of callback arguments! [" + numDependencies + " != " + numCallbackArgs + "]");
+
+    } catch(e) {
+        console.warn("Failure: %s", e);
+        process.exit();
+    }
+
+    console.log("Found nothing obviously wrong with your file");
+
+}) ();
